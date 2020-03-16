874d05e540bbbbad63e7ee93182bec2cfc325d32
Maher Zaydoun <maher.zaydoun@satoripop.com>
2020-03-16 21:54:17 +0100

    Updates

---
+++ precache-manifest.6b38a4de506f412c21efa07e26a2a8d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc07469c4b1e292f8756a56dffc16a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e795f097e9c259b41a1b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "187b7d362409c4341b64",
+    "url": "/static/js/2.128fe4a5.chunk.js"
+  },
+  {
+    "revision": "32ac666b7c7d71f2807a8e3c52e8a26a",
+    "url": "/static/js/2.128fe4a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e795f097e9c259b41a1b",
+    "url": "/static/js/main.9c2bccf4.chunk.js"
+  },
+  {
+    "revision": "23d0f50341623c1f0913",
+    "url": "/static/js/runtime-main.748dab49.js"
+  }
+]);
