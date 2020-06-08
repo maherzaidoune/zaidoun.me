83357fe490c1a61d05f432b34fa75ea55e5cdda1
Maher Zaydoun <maher.zaydoun@satoripop.com>
2020-06-09 00:07:14 +0100

    update text style

--- src/component/About.js
+++ src/component/About.js
@@ -5,67 +5,69 @@ import { Font } from '../Shared/Font';
 
 
 const me = [
-        "Bachelor degree from ESSTHS ",
-        "Studying Software engineering at EPI",
-        "Joined GitHub February 8, 2015 😄",
-        "Active open source community contributor since 2017",
-        "I use Libgdx to make TaniEscape, a game made just for fun  😎 😎 ",
-        "RRDL 😍 ",
-        "Football fan, Mkacha5  🇩🇪",
-        "I don't like to read, but I do listen to a lot of audiobooks, a fan of kitab sawti app",
+    "Bachelor degree from ESSTHS ",
+    "Studying Software engineering at EPI",
+    "Joined GitHub February 8, 2015 😄",
+    "Active open source community contributor since 2017",
+    "I use Libgdx to make TaniEscape, a game made just for fun  😎 😎 ",
+    "RRDL 😍 ",
+    "Football fan, Mkacha5  🇩🇪",
+    "I don't like to read, but I do listen to a lot of audiobooks, a fan of kitab sawti app",
 ]
 export default class About extends PureComponent {
 
-    constructor(props){
+    constructor(props) {
         super(props)
-        this.state  = {
+        this.state = {
             sliderIndex: 0,
             maxSlider: me.length - 1
         }
     }
-scrollToIndex = (index, animated) => {
-   this.listRef && this.listRef.scrollToIndex({ index, animated })
- }
 
- componentDidMount() {  
-   setInterval(function() {
-     const { sliderIndex, maxSlider } = this.state
-     let nextIndex = 0
+    scrollToIndex = (index, animated) => {
+        this.listRef && this.listRef.scrollToIndex({ index, animated })
+    }
 
-     if (sliderIndex < maxSlider) {
-       nextIndex = sliderIndex + 1
-     }
+    componentDidMount() {
+        setInterval(function () {
+            const { sliderIndex, maxSlider } = this.state
+            let nextIndex = 0
 
-     this.scrollToIndex(nextIndex, true)
-     this.setState({sliderIndex: nextIndex})
-   }.bind(this), 1000)
- }
+            if (sliderIndex < maxSlider) {
+                nextIndex = sliderIndex + 1
+            }
 
-    renderMe = ({item}) => {
-        return(
-        <View style={{
-                    margin: 10,
-                    marginEnd: 20,
-                    marginStart: 20,
-                    padding: 30,
-                    height: 130,
-                    width: 240,
-                    borderWidth: 1,
-                    borderRadius: 20,
-                    borderColor: Color.secondary,
-                    justifyContent: 'center',
-                    alignItems: 'flex-start'
-                }}>
-                    <Text 
+            this.scrollToIndex(nextIndex, true)
+            this.setState({ sliderIndex: nextIndex })
+        }.bind(this), 1000)
+    }
+
+    renderMe = ({ item }) => {
+        return (
+            <View style={{
+                flex: 1,
+                margin: 10,
+                marginEnd: 20,
+                marginStart: 20,
+                padding: 30,
+                width: 240,
+                borderWidth: 1,
+                borderRadius: 20,
+                borderColor: Color.secondary,
+                justifyContent: 'center',
+                alignItems: 'flex-start'
+            }}>
+                <Text
                     style={{
                         color: Color.white,
-                        fontFamily: Font.regular,
+                        fontFamily: Font.bold,
+                        fontSize: 14,
                         textAlign: 'start',
                     }}>{item}</Text>
-                </View>
-                )
-                
-            }
+            </View>
+        )
+
+    }
 
     render() {
         return (
@@ -73,19 +75,19 @@ scrollToIndex = (index, animated) => {
                 flex: 1,
                 alignItems: 'center',
                 justifyContent: 'flex-start',
-               
+
             }}>
-               <Text 
+                <Text
                     style={styles.infotext} >
                     I do stuff on my computer that makes life better 😄
                 </Text>
                 <FlatList
-                  ref={listRef => { this.listRef = listRef }}
-                showsHorizontalScrollIndicator={false}
-                style={{
-                    flex: 1,
-                    maxWidth: 400
-                }}
+                    ref={listRef => { this.listRef = listRef }}
+                    showsHorizontalScrollIndicator={false}
+                    style={{
+                        flex: 1,
+                        maxWidth: 400
+                    }}
                     data={me}
                     heigth={200}
                     horizontal
@@ -98,14 +100,14 @@ scrollToIndex = (index, animated) => {
 
 const styles = StyleSheet.create({
     infotext: {
-      color: Color.white,
-      fontFamily: Font.regular,
-      textAlign: 'start',
-      fontSize: 18,
-      padding: 20,
+        color: Color.white,
+        fontFamily: Font.regular,
+        textAlign: 'start',
+        fontSize: 18,
+        padding: 20,
     },
     info: {
-      flex: 1,
-      justifyContent: 'center',
+        flex: 1,
+        justifyContent: 'center',
     }
-  });
+});
